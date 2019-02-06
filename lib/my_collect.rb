f86b36b8a306a7f56d85cfe116b79eca949deaa4
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-06 13:29:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,13 @@
 def my_collect(array)
-  if block_given?
     new_array = []
     i=0
     while i<array.length
-      new_array << yield(array[i])
+      new_array.push yield(array[i])
       i=i+1
-    
+    end
+  new_array
+end
+
+my_collect([]) do |name| name.split(" ").first
+end
 
