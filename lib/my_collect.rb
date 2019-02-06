7979532e7be42d4261f9df3fb5060a3885a1a6ad
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-06 13:39:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+def my_collect(array)
+    new_array = []
+    i=0
+    while i<array.length
+      new_array.push yield(array[i])
+      i=i+1
+    end
+  new_array
+end
+
+students  = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+
+my_collect(students) do |name| 
+  name.split(" ").first
+end
+
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+
+my_collect(languages) do |lang|
+  lang.upcase
+end
+
 
 
